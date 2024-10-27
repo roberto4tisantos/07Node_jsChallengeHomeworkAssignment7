38652cd2b4c2c286b13296e8b1c480ad0d1505c7
FORTIS Roberto <roberto.4ti@gmail.com>
2024-10-27 18:47:33 -0400

    index.js

--- index.js
+++ index.js
@@ -2,111 +2,75 @@
 import inquirer from 'inquirer';
 import fs from 'fs';
 
-// Create an array of names
-const names = [ 'Project title:',
-                'Description:',
-                'Project installation:',
-	        	'Project usage information:',
-				'Project contributing:',
-				'Project tests:' ];
-
 // TODO: Create an array of questions for user input
-const questions = [ 'What is your project title?',
-	'What is project description?',
-	'What is project installation?',
-	'What is project usage information?',
-	'What is project contributing?',
-	'What is project tests?' ];		
-	
-	//Array index
-	let iMessage = -1;
-	let iName = -1;	
-
-//Get array names
-function getArrayNames(iName) {
-	// console.log(`arrayName: ${names[idx]}`);	
-	// iName += 1;	
-	//return name
-	return names[iName];
-}
-
-//Get array questions
-function getArrayQuestions(iMessage) {
-	// console.log(`arrayQuestion: ${questions[idx]}`);
-	// iMessage += 1;
-	//return question
-	return questions[iMessage];
-}
+const questions = [
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the project title?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'What is the project description?',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the project installation instructions?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the project usage information?',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What is the project license?',
+        choices: ['None', 'MIT'],
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'What are the project contribution guidelines?',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the project test instructions?',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is the project GitHub username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is the project email address?',
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
-    // fs.writeFile('index.html', htmlPageContent, (err) =>
-    //   err ? console.log(err) : console.log('Successfully created index.html!')
-    // );
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log('README.md has been created successfully!');
+      });
 }
-
-//WHEN I enter my project title
-//THEN this is displayed as the title of the README
-//WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-//THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-async function inquirerPrompt() {
-
-	// let question = getArrayQuestions();
-	// let answer = prompt(question);
-
-	// // let question = prompt(getArrayQuestions(questions));
-	// let name = getArrayNames();
-	// // console.log(`inquirerPrompt.name: ${name}`);
-	// let qust = getArrayQuestions();  
-	// // console.log(`inquirerPrompt.question: ${qust}`);
-
-	//Array index
-	iMessage ++;
-	iName ++;
-
-	// let answer = await inquirer.prompt([	
-	// inquirer.prompt([		
-	let answers = await inquirer
-		.prompt([		
-			{
-			type: 'input',
-			name: getArrayNames(iName),
-			message: getArrayQuestions(iMessage),
-			validate: (input) => input ? true : 'answer cannot be empty.',		  		  
-			}	
-	  	// .then( (answers) => {
-		// 	//const writeToFile = writeToFile('README.MD', answers);
-		// 	console.log('answer:', answers);	 
-		// })				
-	  	])
-
-	  	// .then( (answers) => {
-		// 	//const writeToFile = writeToFile('README.MD', answers);
-		// 	console.log('answer:', answers);	 
-		// });		  
-
-	// console.log(`answers: ${answers}`);
-
-	// const answers = await inquirer.prompt(questions);
-	// console.log('Repository Information:', answers);	 
-
-	//Increase index
-	// console.log(`Index of Array: ${idx}`)	
-	
-
-}
-
 
 // TODO: Create a function to initialize app
 function init() {
-	//Loop through each question
-	questions.forEach(inquirerPrompt);
+    inquirer.prompt(questions)
+            .then((answers) => {
+              const markdown = generateMarkdown(answers);
+              writeToFile('README.md', markdown);
+            });
 }
 
 // Function call to initialize app
 init();
-
-
-
-

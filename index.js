f291b5fe4b93603499a5b9ad9f2c6326dd9b9d64
FORTIS Roberto <roberto.4ti@gmail.com>
2024-10-27 13:19:18 -0400

    index.js

--- index.js
+++ index.js
@@ -2,35 +2,33 @@
 import inquirer from 'inquirer';
 import fs from 'fs';
 
+// TODO: Create an array of questions for user input
+const questions = [ 'What is your project title?',
+					'What is project description?',
+					'What is project installation?',
+					'What is project usage information?',
+					'What is project contributing?',
+					'What is project tests?' ];
+
 // Create an array of names
 const names = [ 'Project title:',
                 'Description:',
                 'Project installation:',
-	        'Project usage information:',
-		'Project contributing:',
-		'Project tests:' ];
+	        	'Project usage information:',
+				'Project contributing:',
+				'Project tests:' ];
 
-// TODO: Create an array of questions for user input
-const questions = [ 'What is your project title?',
-                    'What is project description?',
-                    'What is project installation?',
-	            'What is project usage information?',
-		    'What is project contributing?',
-		    'What is project tests?' ];
-
-"Array index
+//Array index
 let index = 0;
 
 //Get array names
 function getArrayNames(arrayNames) {
-
 	//return name
 	return arrayNames[index];
 }
 
 //Get array questions
 function getArrayQuestions(arrayQuestions) {
-
 	//return question
 	return arrayQuestions[index];
 }
@@ -43,35 +41,39 @@ function writeToFile(fileName, data) {
     );
 }
 
-
 //WHEN I enter my project title
 //THEN this is displayed as the title of the README
 //WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
 //THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-function inquirerPrompt() {
+async function inquirerPrompt() {
 
-	inquirer
-	  .prompt([
+	inquirer.prompt([
     	{
 	      type: 'input',
-	      name: getArrayNames(names)),
-	      message: getArrayQuestions(questions)),
-	    },
+	      name: getArrayNames(names),
+	      message:  prompt(getArrayQuestions(questions)),
+		  validate: (input) => input ? true : 'Repository name cannot be empty.',
+	    }
 	  ])
 
-	//.then( (answers) => {
-	//    const writeToFile = writeToFile('README.MD', answers);
+	// .then( (answers) => {
+	// //    const writeToFile = writeToFile('README.MD', answers);
+	// 	console.log('Repository Information:', answers);	 
 	//  });
+
+	// const answers = await inquirer.prompt(questions);
+	// console.log('Repository Information:', answers);	 
+
+	//Increase index
+	console.log(`Index of Array: ${index}`)	
+	index += 1;	
+}
 
 
 // TODO: Create a function to initialize app
 function init() {
-
 	//Loop through each question
 	questions.forEach(inquirerPrompt);
-
-	//Increase index
-	index += 1;
 }
 
 // Function call to initialize app

f95f65ce90c213dd5d08a5b0d42371a717f6d518
FORTIS Roberto <roberto.4ti@gmail.com>
2024-10-30 17:28:40 -0400

    generateMarkdown.js modified

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,19 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-
+  if (!license || license === 'None') return '';
+  return `![License](https://img.shields.io/badge/license-${license}-blue.svg)`;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-
+  if (!license || license === 'None') return '';
+  return `* [License](#license)`;
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
+  if (!license || license === 'None') return '';
+  return `## License\nThis project is licensed by ${license} license.`;
 }
 
 // TODO: Create a function to generate markdown for README
